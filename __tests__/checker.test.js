09459ae6bcc76e8d97d99ee43056666834db30ee
Liz Thomas <thomas.elizabeth.k@gmail.com>
2021-11-09 13:41:34 -0800

    checkArray() Test 1 created.
    Co-authored-by: Christy Welch <christinamawel@gmail.com>
    Co-authored-by: Anna Pittman <annaepittman@gmail.com>

--- __tests__/checker.test.js
+++ __tests__/checker.test.js
@@ -1,8 +1,16 @@
-import differnetNums from './../src/checker.js';
+import { differnetNums } from './../src/checker.js';
+import { checkArray } from './../src/checker.js';
 
 describe(differnetNums, () => {
 
   test('should take an array of numbers and return true if they are all different', () => {
     expect(differnetNums([1,2,3,4,5,6,7,8,9])).toEqual(true); 
+  })
+})
+
+describe(checkArray, () => {
+
+  test('It should check one array to see if there is a false in that array', () => {
+    expect(checkArray[true, false, false, true]).toEqual(false);
   })
 })
