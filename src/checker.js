9ee5a0aa576e94829ec374bb09877d44a35d2bd6
Liz Thomas <thomas.elizabeth.k@gmail.com>
2021-11-09 13:28:44 -0800

    differentNums() Test 1 passed: Returns true.
    Co-authored-by: Christy Welch <christinamawel@gmail.com>
    Co-authored-by: Anna Pittman <annaepittman@gmail.com>

--- src/checker.js
+++ src/checker.js
@@ -1,4 +1,12 @@
 export default function differnetNums(array) {
-  return
+  let arrayNums = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  for (let i = 1; i < array.length; i++) {
+    if (array.includes(array[i])) {
+      arrayNums.splice(arrayNums.indexOf(array[i]), 1);
+    } else {
+        return false;
+    }
+  }
+  return true;
 };
 
